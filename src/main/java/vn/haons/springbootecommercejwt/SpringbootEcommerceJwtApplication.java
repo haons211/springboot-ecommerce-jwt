9339c20361c0de0b62abbd9d99815af67d7a803d
haons211 <syhaoc2dh@gmail.com>
2024-07-11 23:33:30 +0700

    first commit

---
+++ src/main/java/vn/haons/springbootecommercejwt/SpringbootEcommerceJwtApplication.java
@@ -0,0 +1,13 @@
+package vn.haons.springbootecommercejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcommerceJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEcommerceJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/haons/springbootecommercejwt/SpringbootEcommerceJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.haons.springbootecommercejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcommerceJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
